13fbef6c303bdca7f8a0d24155b4053be5a6ffbd
rkeerthirajagopal <keerthigopal11@gmail.com>
2023-01-11 15:13:33 +0530

    name changed

--- src/List.js
+++ src/List.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const List = ({ people }) => {
+const List = ({ peo }) => {
   return (
     <>
-      {people.map((person) => {
+      {peo.map((person) => {
         const { id, name, age, image } = person;
         return (
           <article key={id} className='person'>
--- src/data.js
+++ src/data.js
@@ -1,36 +1,36 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Keerthi',
+    age: 19,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Harthi',
+    age: 22,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
+    name: 'Tommy',
     age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
+    name: 'Ganesha',
     age: 34,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
+    name: 'Lola',
+    age: 21,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
